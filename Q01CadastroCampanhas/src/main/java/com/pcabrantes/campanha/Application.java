d115ee9463c0f0aeb8b84dcc14ef70a76b06f05a
Paulo Cesar <pc.abrantes@gmail.com>
2017-05-05 22:08:52 -0300

    closes #1 Criação de projeto Maven + Spring Boot

---
+++ Q01CadastroCampanhas/src/main/java/com/pcabrantes/campanha/Application.java
@@ -0,0 +1,21 @@
+package com.pcabrantes.campanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe responsável por iniciar a aplicação
+ *
+ *
+ * @author Paulo Cesar Abrantes
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+}
